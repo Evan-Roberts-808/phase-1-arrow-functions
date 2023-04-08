097cf600928f8aeea09ad159ea4f902343bfa5a1
Evan <eroberts1998@icloud.com>
2023-04-08 18:38:23 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function() {
+    return 2000 / 100;
+};
+
+const square = (value) => {
+    return value * value;
+};
+
+const add = (param1, param2) => {
+    return param1 + param2;
+}
